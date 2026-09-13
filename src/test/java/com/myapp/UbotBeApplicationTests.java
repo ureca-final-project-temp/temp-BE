b2604e6d5f1019e563fde8b165a98b59db28acc4
Catverdose <eongpup@gmail.com>
2026-09-13 11:08:40 +0900

    chore: 로컬 개발 인프라 및 CI 환경 구성

---
+++ src/test/java/com/myapp/UbotBeApplicationTests.java
@@ -0,0 +1,14 @@
+package com.myapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class UbotBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
